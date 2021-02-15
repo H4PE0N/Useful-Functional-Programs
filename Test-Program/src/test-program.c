f05b88f6b1ecff726e451202a8ec10bd40800594
Hampus Fridholm <hampus.fridholm@gmail.com>
2021-02-15 16:50:03 +0100

    Made some progress on the string-morse program. This branch is for this program only.

--- Test-Program/src/test-program.c
+++ Test-Program/src/test-program.c
@@ -9,24 +9,126 @@
 
 #include "test-program.h"
 
-int main(int count, char* arguments[])
+int main(int arg_length, char* arguments[])
 {
-  char* string = arguments[1];
-  int length = strlen(string);
+  const int file_lines = 36, line_size = 200;
+  char*** morse_table = generate_morse_table(file_lines, line_size);
 
-  char* reversed = reverse_string_variable(string, length);
+  if(arg_length >= 3 && !strcmp(arguments[1], "morse"))
+  {
+    char* string = arguments[2];
+    int length = strlen(string);
+    char** morse_string = convert_string_morse(morse_table, file_lines, string, length);
+    output_morse_string(morse_string, length);
+  }
+  else if(arg_length >= 3 && !strcmp(arguments[1], "string"))
+  {
+    char** morse_string = extract_morse_string(arguments, arg_length);
+    int length = arg_length - 2;
 
-  printf("%s REVERSED: %s\n", string, reversed);
+    char* string = convert_morse_string(morse_table, file_lines, morse_string, length);
+    printf("%s\n", string);
+  }
 
   return 0;
 }
 
-char* reverse_string_variable(char* string, int length)
+char** extract_morse_string(char* arguments[], int arg_length)
 {
-  char* reversed = malloc(sizeof(char) * length);
+  int length = (arg_length - 2);
+  char** morse_strings = malloc(sizeof(char*) * length);
+  for(int index = 0; index < arg_length - 2; index = index + 1)
+  {
+    char* morse_char = *(arguments + index + 2);
+    *(morse_strings + index) = morse_char;
+  }
+  return morse_strings;
+}
+
+void output_morse_string(char** morse_string, int length)
+{
   for(int index = 0; index < length; index = index + 1)
   {
-    *(reversed + index) = *(string + (length-index-1));
+    printf("%s ", *(morse_string + index));
   }
-  return reversed;
+  printf("\n");
+}
+
+char* convert_morse_string(char*** table, int lines, char** morse, int length)
+{
+  char* string = malloc(sizeof(char) * length);
+  for(int index = 0; index < length; index = index + 1)
+  {
+    char* current_morse = *(morse + index);
+    char character = convert_morse_character(table, lines, current_morse);
+    *(string + index) = character;
+  }
+
+  return string;
+}
+
+char** convert_string_morse(char*** table, int lines, char* string, int length)
+{
+  char** morse = malloc(sizeof(char*) * length);
+  for(int index = 0; index < length; index = index + 1)
+  {
+    char character = *(string + index);
+    char* current_code = convert_character_morse(table, lines, character);
+    *(morse + index) = current_code;
+  }
+  return morse;
+}
+
+char convert_morse_character(char*** table, int lines, char* morse)
+{
+  char character = '\0';
+  for(int index = 0; index < lines; index = index + 1)
+  {
+    char** letter_code = *(table + index);
+    if(strcmp(*(letter_code + 1), morse)) continue;
+    character = *(*(letter_code + 0) + 0); break;
+  }
+  return character;
+}
+
+char* convert_character_morse(char*** table, int lines, char character)
+{
+  char* morse_code = malloc(sizeof(char) * 10);
+  for(int index = 0; index < lines; index = index + 1)
+  {
+    char** letter_code = *(table + index);
+    if(*(*(letter_code + 0) + 0) != character) continue;
+    morse_code = *(letter_code + 1); break;
+  }
+  return morse_code;
+}
+
+char*** generate_morse_table(int lines, int line_size)
+{
+  char** file_lines = extract_file_information("../src/data.csv", lines, line_size);
+  char*** morse_table = malloc(sizeof(char**) * lines);
+  char* seperator = ",";
+  for(int index = 0; index < lines; index = index + 1)
+  {
+    char* current_line = *(file_lines + index);
+    char** letter_code = malloc(sizeof(char*) * 2);
+    *(letter_code + 0) = strtok(current_line, seperator);
+    *(letter_code + 1) = strtok(NULL, seperator);
+    *(morse_table + index) = letter_code;
+  }
+  return morse_table;
+}
+
+char** extract_file_information(char* file_name, int lines, int line_size)
+{
+  char** file_lines = malloc(sizeof(*file_lines) * lines);
+  FILE* file_pointer = fopen(file_name, "r");
+  char current_line[line_size];
+  for(int index = 0; index < lines; index = index + 1)
+  {
+    if(!fgets(current_line, line_size, file_pointer)) break;
+    int current_length = strlen(current_line);
+    file_lines[index] = strncpy( malloc( line_size ), current_line, current_length - 1);
+  }
+  return file_lines;
 }
--- Test-Program/src/test-program.h
+++ Test-Program/src/test-program.h
@@ -2,6 +2,20 @@
 #ifndef TEST_PROGRAM_H
 #define TEST_PROGRAM_H
 
-char* reverse_string_variable(char*, int);
+char*** generate_morse_table(int, int);
+
+char* convert_character_morse(char***, int, char);
+
+char convert_morse_character(char***, int, char*);
+
+char** convert_string_morse(char***, int, char*, int);
+
+char** extract_file_information(char*, int, int);
+
+char* convert_morse_string(char***, int, char**, int);
+
+char** extract_morse_string(char*[], int);
+
+void output_morse_string(char**, int);
 
 #endif
